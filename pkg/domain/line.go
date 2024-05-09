bc29589e7d29077622d99f3479f66b44c73b9828
RafaelFino <rafael.gottardi@gmail.com>
2024-05-09 11:21:18 -0300

    imp - new domain.data

--- pkg/domain/line.go
+++ pkg/domain/line.go
@@ -1,6 +1,7 @@
 package domain
 
 type Line interface {
+	SetSchema(schema string) error
 	Decode(data map[interface{}]interface{}) error
 	Headers() []string
 	ToString() string
--- pkg/domain/log.go
+++ pkg/domain/log.go
@@ -9,6 +9,10 @@ func NewLog() Line {
 	return &Log{}
 }
 
+func (l *Log) SetSchema(schema string) error {
+	return nil
+}
+
 func (l *Log) ToString() string {
 	return fmt.Sprintf("%+v", l)
 }
