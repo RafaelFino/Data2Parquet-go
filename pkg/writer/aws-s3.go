319d98be53e641dab100478a6fc70084c694f1a0
RafaelFino <rafael.gottardi@gmail.com>
2024-05-13 16:24:19 -0300

    WIP - AWS-S3 Writer

--- pkg/writer/aws-s3.go
+++ pkg/writer/aws-s3.go
@@ -1,13 +1,21 @@
 package writer
 
 import (
+	"context"
 	"data2parquet/pkg/config"
 	"data2parquet/pkg/domain"
+	"errors"
 	"log/slog"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awsConfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 type S3 struct {
 	config *config.Config
+	client *s3.Client
 }
 
 func NewS3(config *config.Config) Writer {
@@ -17,20 +25,55 @@ func NewS3(config *config.Config) Writer {
 }
 
 func (s *S3) Init() error {
-	slog.Debug("[writer] Initializing S3 writer", "config", s.config.ToString())
+	slog.Debug("Initializing S3 writer", "config", s.config.ToString())
+
+	// Load the Shared AWS Configuration (~/.aws/config)
+	awsCfg, err := awsConfig.LoadDefaultConfig(context.TODO())
+
+	if err != nil {
+		slog.Error("Error loading AWS config", "error", err)
+		return err
+	}
+
+	// Create an Amazon S3 service client
+	slog.Debug("Creating S3 client")
+	s.client = s3.NewFromConfig(awsCfg)
+
+	if s.client == nil {
+		slog.Error("Error creating S3 client")
+		return errors.New("Error creating S3 client")
+	}
+
 	return nil
 }
 
 func (s *S3) Write(data []*domain.Record) error {
-	slog.Debug("[writer] Writing logs", "data", data)
-	return nil
+	slog.Debug("Writing logs", "data", data)
+
+	file, err := os.Open("data2parquet.parquet")
+
+	if err != nil {
+		slog.Error("fail to try open file")
+	}
+
+	bucketName := ""
+	objectKey := ""
+
+	defer file.Close()
+	_, err = s.client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+		Body:   file,
+	})
+
+	return err
 }
 
 func (s *S3) Close() error {
-	slog.Debug("[writer] Closing S3 writer")
+	slog.Debug("Closing AWS-S3 writer")
 	return nil
 }
 
 func (s *S3) IsReady() bool {
-	return true
+	panic("NOT IMPLEMENTED YET!!!")
 }
